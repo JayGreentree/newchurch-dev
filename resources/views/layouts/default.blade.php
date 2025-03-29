a331dc949faa49f91417eebe5cbc51dd146f9482
jaygreentree <jgreentreevin@gmail.com>
2025-03-29 03:35:13 -0500

    Update default.blade.php

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -41,7 +41,7 @@
                             <li class="nav-item"><a class="nav-link" href="{{ url('/pastor')}}">Pastors </a></li>
                             <li class="nav-item"><a class="nav-link" href="{{ url('/sermon')}}">Sermons</a></li>
                             <li class="nav-item"><a class="nav-link" href="{{ url('/events')}}">Events</a></li>
-                            <li class="nav-item current-nav-item"><a href="{{ url('/about') }}">About</a></li>
+                            <li class="nav-item"><a class="nav-link" href="{{ url('/about') }}">About</a></li>
                             <li class="nav-item"><a class="nav-link" href="{{ url('/contact')}}">Contact</a></li>
                     </ul>
                 </div>
